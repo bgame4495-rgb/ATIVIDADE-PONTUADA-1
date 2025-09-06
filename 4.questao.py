623484b1b98f5ba445d56f162377f3e8214a8cc3
BrunoMAT448 <bgame4495@gmail.com>
2025-09-06 12:52:31 -0300

    Update 4.questao.py

--- 4.questao.py
+++ 4.questao.py
@@ -1,15 +1,34 @@
 import os
 os.system ("cls")
 
-peso = 5 
-maca = int(input (f"QUANTAS MAÇAS COMPRAR: "))
+maca_kg = float(input("Quantidade de maça (kg): "))
+morango_kg = float(input("quantidade de morango (kg): "))
 
-
-if maca <= 5:
-    preco_unitario = 1.50 
-
+if maca_kg <= 5:
+	preco_maca =1.80
 else:
-    preco_unitario = 1.80
+	preco_maca =1.50
+	
+if morango_kg <= 5:
+	preco_morango = 2.50
+else:
+	preco_morango = 2.20
+	
+#calculo
+	
+total_maca = preco_maca * maca_kg
+total_morango = preco_morango * morango_kg 
+total= preco_maca + preco_morango
 
-valor_total = maca * preco_unitario
-print(f"preço total: {valor_total:.2f}")
+#desconto
+
+peso_total = maca_kg + morango_kg
+if peso_total > 10 or total > 15:
+	total = total * 0.90
+	
+#final
+
+print(f"morango: {morango_kg}kg em R$ {total_morango}")
+print(f"maça: {maca_kg}kg em para R$ {total_maca}")
+print(f"total R$: {total}")
+
