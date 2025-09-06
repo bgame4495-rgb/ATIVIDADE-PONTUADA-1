33ff07fa6f68a28d2ef8a2381f5e01e2410a4d3d
BrunoMAT448 <bgame4495@gmail.com>
2025-09-06 10:28:47 -0300

    Update 2.questao.py

--- 2.questao.py
+++ 2.questao.py
@@ -8,7 +8,7 @@ estado_civil = input("Digite o estado civil: ")
 
 tempo_casada = None
 
-if sexo == "F" and estado_civil == "CASADA":
+if sexo == "F" and estado_civil == "casada":
     tempo_casada = int(input("Digite o tempo de casada: "))
 
 print("Dados do usuário")
@@ -18,3 +18,4 @@ print("Estado civil:", estado_civil)
 
 if tempo_casada is not None:
     print("Tempo de casada:", tempo_casada, "anos")
+
