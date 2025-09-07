57e897219fce92e00911f3e9f1ca4f61e5dc4dfa
BrunoMAT448 <bgame4495@gmail.com>
2025-09-07 00:33:14 -0300

    Criar 8.questao.py

---
+++ 8.questao.py
@@ -0,0 +1,17 @@
+
+import os
+os.sytem("cls")
+
+cor= input("Escolha a cor desejada: ")
+
+match cor:
+	case "verde":
+		print("CD da faixa verde R$:10.00")
+	case "azul":
+		print("CD da faixa azul R$20.00")
+	case "amarelo":
+		print("CD da faixa Amarela R$30.00")
+	case "vermelho":
+		print("CD da faixa Vermelha R$40.00")
+	case _:
+		print("Cor invalida")
